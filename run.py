090901ac3cc1e28f8a2a2df35ffb4a6b7859e14f
T-Ash-90 <thomasedwardash@pm.me>
2026-03-30 21:39:08 +0200

    local hosting only

--- run.py
+++ run.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
 
         uvicorn.run(
             "backend.server:app",
-            host="0.0.0.0",
+            host="127.0.0.1",
             port=8000,
             reload=False,
         )
